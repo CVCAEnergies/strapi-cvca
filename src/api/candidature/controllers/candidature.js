39b1599e26bc348e0558ba74859ff9659ce9a3ca
Nathan Crincket <crincketnathan@gmail.com>
2024-12-28 21:30:52 +0100

    mail-v3

--- src/api/candidature/controllers/candidature.js
+++ src/api/candidature/controllers/candidature.js
@@ -8,21 +8,4 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::candidature.candidature');
 
-module.exports = {
-    async testLifecycle(ctx) {
-      try {
-        const result = { name: 'Test', email: 'test@exemple.com' };
-  
-        await strapi.plugin('email').service('email').send({
-          to: 'nathan.crincket@etu.imt-nord-europe.fr',
-          subject: 'Test Email depuis testLifecycle',
-          text: `Voici un email de test avec les données suivantes : ${JSON.stringify(result)}`,
-        });
-  
-        ctx.send({ message: 'Email envoyé avec succès depuis testLifecycle.' });
-      } catch (err) {
-        ctx.send({ error: err.message });
-      }
-    },
-  };
-  
+
--- src/api/candidature/routes/candidature.js
+++ src/api/candidature/routes/candidature.js
@@ -8,13 +8,3 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::candidature.candidature');
 
-module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/test-lifecycle',
-        handler: 'candidature.testLifecycle',
-      },
-    ],
-  };
-  
