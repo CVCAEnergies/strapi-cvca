57fc140ec0e844f44de2e80716754257816ecd13
Nathan Crincket <crincketnathan@gmail.com>
2024-12-28 22:14:59 +0100

    email-v12000

--- src/api/candidature/content-types/candidature/lifecycles.js
+++ src/api/candidature/content-types/candidature/lifecycles.js
@@ -14,24 +14,7 @@ module.exports = {
         const tel = result.tel || 'Non renseigné';
         const message = result.message || 'Non renseigné';
     
-        // Récupérer les fichiers de type CV et lettre de motivation
-        const cv = result.cv && result.cv.url ? result.cv.url : null;
-        const motiv = result.motiv && result.motiv.url ? result.motiv.url : null;
-    
-        // Préparer les pièces jointes
-        const attachments = [];
-        if (cv) {
-          attachments.push({
-            filename: 'cv.pdf', // ou le nom du fichier CV
-            path: cv, // URL du fichier CV
-          });
-        }
-        if (motiv) {
-          attachments.push({
-            filename: 'lettre_de_motivation.pdf', // ou le nom du fichier Motiv
-            path: motiv, // URL du fichier Motiv
-          });
-        }
+        
     
         // Envoi de l'email avec les pièces jointes
         await strapi.plugin('email').service('email').send({
@@ -45,8 +28,8 @@ module.exports = {
                    <li><strong>Email :</strong> ${email}</li>
                    <li><strong>Téléphone :</strong> ${tel}</li>
                    <li><strong>Message :</strong> ${message}</li>
-                 </ul>`,
-          attachments: attachments, // Ajouter les pièces jointes
+                 </ul>
+                 <p>Rendez-vous sur le site pour télécharger le CV et la lettre de motivation : <a href="https://rational-flowers-37168cb5d5.strapiapp.com/admin/auth/login">Ici</a></p>`
         });
       } catch (err) {
         console.log(`Erreur lors de l'exécution du lifecycle : ${err.message}`);
